9cf643d3ded2a9fd5ae23c19880f15d2710f1d85
[xct24] <[xct24@live.com]>
2020-07-23 20:12:31 +0800

    完成了GUI的雏形和一些调试工具，已经实现可控识别和输出对方阵容。 完成内容： 1. config.py添加apiKey和region字段 2. data.py填入了所有的数据 3. 添加了GUI（未完全完成） 4. 添加了作业相关GUI Widget（未完全完成） 5. Util的一些bug修复 6. 请求数据的基础框架 后续更新计划： 1. 完成请求数据和渲染数据的环节 2. 添加收藏和自定义阵容功能，不再必须每次都去远端请求数据。

--- main.py
+++ main.py
@@ -1,10 +1,126 @@
 import util
+import json
+import time
 import config
+import data
 import cv2
+import sys
+import httpx
+import asyncio
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QLabel, QWidget
+from PyQt5 import QtGui, QtCore, QtNetwork
+from gui import Ui_PCRJJCAnalyzerGUI
+from solutionWidget import Ui_solutionWidget
 global handle
+global charImageList
+global charDataList
+global apiKey
+
+class GUIsolutionWidget(QWidget, Ui_solutionWidget):
+    def __init__(self, parent=None, solution):
+        super(GUIsolutionWidget, self).__init__(parent)
+        self.setupUi(self)
+        
+
+class GUIMainWin(QMainWindow, Ui_PCRJJCAnalyzerGUI):
+    def __init__(self, parent=None):
+        super(GUIMainWin, self).__init__(parent)
+        self.setupUi(self)
+        self.Debugger.clicked.connect(self.showChars)
+        self.parse.clicked.connect(self.parseChars)
+        self.query.clicked.connect(self.do_query)
+        self.tryAddSolution.clicked.connect(self.addSolution)
+    def addSolution(self, solution):
+        self.solutionListLayout.addWidget(GUIsolutionWidget(solution))
+        self.solutionListScrollAreaScrollAreaWidgetContents.setLayout(self.solutionListLayout)
+    def showChars(self):
+        self.pixCharImageList = [QtGui.QPixmap.fromImage(charImage) for charImage in charImageList]
+        self.itemCharImageList = [QGraphicsPixmapItem(pix) for pix in self.pixCharImageList]
+        # self.sceneCharImageList = [QGraphicsScene().addItem(item) for item in self.itemCharImageList]
+        self.sceneCharImageList = [QGraphicsScene(), QGraphicsScene(), QGraphicsScene(), QGraphicsScene(), QGraphicsScene()]
+        for i in range(len(self.sceneCharImageList)):
+            self.sceneCharImageList[i].addItem(self.itemCharImageList[i])
+        self.char1Avatar.setScene(self.sceneCharImageList[0])
+        self.char2Avatar.setScene(self.sceneCharImageList[1])
+        self.char3Avatar.setScene(self.sceneCharImageList[2])
+        self.char4Avatar.setScene(self.sceneCharImageList[3])
+        self.char5Avatar.setScene(self.sceneCharImageList[4])
+    def parseChars(self):
+        refImage = cv2.imread('refImage.png') # 读取参考图
+        for i in range(len(charImageList)):
+            charNum = i+1
+            charIndex = (util.cv_getIndex(util.cv_getMidPoint(charImageList[i], refImage, eval('cv2.TM_CCOEFF' )))) # 计算出目标角色在参考图中的坐标位置（行与列）
+            charDataList[i] = data.refGrid[(charIndex[1]-1)][(charIndex[0]-1)]
+            charName = charDataList[i]['name']
+            print(charNum, charName, charIndex)
+    def handleResponse(self, response):
+        er = response.error()
+
+        if er == QtNetwork.QNetworkReply.NoError:
+
+            bytes_string = response.readAll()
+
+            json_ar = json.loads(str(bytes_string, 'utf-8'))
+            data = json_ar['form']
+
+            print('Name: {0}'.format(data['name']))
+            print('Age: {0}'.format(data['age']))
+
+            print()
+
+        else:
+            print("Error occurred: ", er)
+            print(response.errorString())
+    def do_query(self):
+        async def request(id_list):
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36',
+                'authorization': config.apiKey,
+                'Content-Type': 'application/json'
+            }
+            async with httpx.AsyncClient() as client:
+                resp = await client.post('https://api.pcrdfans.com/x/v1/search',json={"_sign": "a", "def": id_list, "nonce": "a", "page": 1, "sort": 1, "ts": int(time.time()), "region": config.region}, headers=headers)
+                result = resp.json()
+            return result
+        raw_id_list = [charData['id'] for charData in charDataList]
+        id_list = [ x * 100 + 1 for x in raw_id_list ]
+        output=asyncio.run(request(id_list))
+        print(output)
+
+        
+        # try:
+        #     resp = await aiorequests.post('https://api.pcrdfans.com/x/v1/search', headers=header, json=payload, timeout=10)
+        #     res = await resp.json()
+        #     logger.debug(f'len(res)={len(res)}')
+        # except Exception as e:
+        #     logger.exception(e)
+        #     return None
+
+        # if res['code']:
+        #     logger.error(f"Arena query failed.\nResponse={res}\nPayload={payload}")
+        #     return None
+
+        # ret = []
+        # for entry in res['data']['result']:
+        #     eid = entry['id']
+        #     likes = get_likes(eid)
+        #     dislikes = get_dislikes(eid)
+        #     ret.append({
+        #         'qkey': gen_quick_key(eid, user_id),
+        #         'atk': [ chara.fromid(c['id'] // 100, c['star'], c['equip']) for c in entry['atk'] ],
+        #         'up': entry['up'],
+        #         'down': entry['down'],
+        #         'my_up': len(likes),
+        #         'my_down': len(dislikes),
+        #         'user_like': 1 if user_id in likes else -1 if user_id in dislikes else 0
+        #     })
+
+    
+        
+    
 
 if __name__ == '__main__':
-    ### CLI测试部分
+    # ### CLI测试部分
     util.gui_promtHandle()  # 输出窗口句柄和标题
     handle = int(input(["请输入句柄"]))
     screenshot = util.gui_getScreenshotByHandle(handle)
@@ -17,16 +133,17 @@ if __name__ == '__main__':
     translatedCharH = gameImage.height()*config.charLocationRatioConfig['h']
     translatedCharW = gameImage.width()*config.charLocationRatioConfig['w']
     charImageList = [gameImage.copy(gameImage.width() * x, translatedCharY, translatedCharW, translatedCharH) for x in config.charLocationRatioConfig['x'] ] # 裁剪出对方每个角色头像
-    refImage = cv2.imread('refImage.png') # 读取参考图
-    for i in range(len(charImageList)):
-        charNum = i+1
-        charIndex = [(util.cv_getIndex(util.cv_getMidPoint(charImageList[i], refImage, eval('cv2.TM_SQDIFF' ))))] # 计算出目标角色在参考图中的坐标位置（行与列）
-        charImageList[i].save("char%s.jpg" % i) # 为了检测是否成功裁剪保存图片
-        print(charIndex, charNum) 
+    charDataList = [
+        {'name': '未知角色', 'id': 1000},
+        {'name': '未知角色', 'id': 1000},
+        {'name': '未知角色', 'id': 1000},
+        {'name': '未知角色', 'id': 1000},
+        {'name': '未知角色', 'id': 1000},
+]
  
 
-    # 还没完成的Qt部分
-    # app = QApplication(sys.argv)
-    # myWin = MyWindow()
-    # myWin.show()
-    # sys.exit(app.exec_())
+
+    app = QApplication(sys.argv)
+    mainWin = GUIMainWin()
+    mainWin.show()
+    sys.exit(app.exec_())
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import win32gui
+import numpy as np
+from matplotlib import pyplot as plt
 import config
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import *
@@ -21,6 +23,7 @@ def cv_getMidPoint(incomingImage, refImage, method):
     targetImage = np.array(ptr).reshape(h, w, 4) #此处完成转换
     targetImage = cv2.cvtColor(targetImage, cv2.COLOR_BGRA2BGR)
 
+
     res = cv2.matchTemplate(refImage,targetImage,method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
@@ -29,6 +32,19 @@ def cv_getMidPoint(incomingImage, refImage, method):
         top_left = min_loc
     else:
         top_left = max_loc
+    
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    cv2.rectangle(refImage,top_left, bottom_right, 255, 2)
+
+    ## 以下用于Debug时生成匹配结果对比图
+    # plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122),plt.imshow(refImage,cmap = 'gray')
+    # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    # plt.suptitle(method)
+    # plt.show()
+
     midPoint = (top_left[0] + 0.5 * w, top_left[1] + 0.5 *h)
     return midPoint
 
@@ -39,7 +55,7 @@ def cv_getIndex(midPoint):
     totalHeight = config.refImg['Height']
     singleWidth = totalWidth / config.refImg['ColumnCount']
     singleHeight = totalHeight / config.refImg['RowCount']
-    index = (int(midPoint[0]/singleWidth)+1, int(midPoint[1]/singleHeight)+1)
+    index = [int(midPoint[0]/singleWidth)+1, int(midPoint[1]/singleHeight)+1]
     return index
 
 
@@ -61,3 +77,24 @@ def gui_getScreenshotByHandle(handle:int):
     screen = app.primaryScreen()
     img = screen.grabWindow(handle).toImage()
     return img
+
+
+
+def query_gen_quick_key(true_id:str, user_id:int) -> str:
+    qkey = int(true_id[-6:], 16)
+    while qkey in quick_key_dic and quick_key_dic[qkey] != true_id:
+        qkey = (qkey + 1) & 0xffffff
+    quick_key_dic[qkey] = true_id
+    mask = user_id & 0xffffff
+    qkey ^= mask
+    return base64.b32encode(qkey.to_bytes(3, 'little')).decode()[:5]
+
+def query_parse_result(result):
+    plan = {
+        'id': result['id'],
+        'planCharList' : [],
+        'planPickList': [],
+        'updated': '',
+        'up': 0,
+        'down': 0,
+    }

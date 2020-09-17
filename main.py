773eef011aa3543ddc6c085bb5cf4780b2514156
[xct24] <[xct24@live.com]>
2020-09-18 00:50:56 +0800

    版本号更新

--- main.py
+++ main.py
@@ -435,7 +435,7 @@ class GUIMainWin(QMainWindow, Ui_PCRJJCAnalyzerGUI):
         super(GUIMainWin, self).__init__(parent)
         self.setupUi(self)
         self.appExceptionHandler = ExceptHookHandler(self, logFile=os.path.join(os.path.expanduser('~'), "PCRJJCAnalyzer", "log.txt"))
-        self.setWindowTitle('PCRJJCAnalyzer-v0.1.4-beta2')
+        self.setWindowTitle('PCRJJCAnalyzer-v0.1.4-beta3')
         self.exclusionList  = [[], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
         self.excludingSolutionIDList = ['','','']
         self.exclusionCheckBoxButtonGroup = QButtonGroup()
